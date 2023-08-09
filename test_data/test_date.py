291117e7a66cf1451bf63335c651651efd61ed1b
H1997Y <2216792443@qq.com>
2023-08-09 11:21:22 +0800

    test_data

---
+++ test_data/test_date.py
@@ -0,0 +1,5 @@
+# 地址放在 字典{} 中   多个字典可放进 列表list[] 中
+url = {"url": "http://erp.lemfix.com"}
+# login_date = [{"username": "test123"}, {"password": "123456"}]
+login_date = {"username": "test123", "password": "123456"}    #键值对形式
+s_key = {"s_key": "9247"}
